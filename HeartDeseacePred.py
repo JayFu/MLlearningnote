05947f7ca217487a147110e31c2c4f5efef0d833
JayFu <382603080@qq.com>
2018-04-26 22:41:20 +0200

    4.26

--- HeartDeseacePred.py
+++ HeartDeseacePred.py
@@ -1,17 +1,16 @@
 # 根据此前R语言所完成的内容，使用sklearn对心脏疾病进行预测
+from sklearn.feature_extraction import DictVectorizer  
+import csv  
+from sklearn import preprocessing  
+from sklearn import tree  
+from sklearn.externals.six import StringIO  
+# from __builtin__ import str  
+from _csv import reader  
+from docutils.nodes import header
 
-from __future__ import print_function
-import sklearn
-import numpy
-from sklearn import datasets
-from sklearn.cross_validation import train_test_split
-from sklearn import tree
-import csv
-from _csv import reader
-
-HeartDeseaseData = open(r'/Users/jay_fu/MLlearningnote/Heart.csv','rb')
+HeartDeseaseData = open(r'/Users/jay_fu/MLlearningnote/Heart.csv','rt')
 reader = csv.reader(HeartDeseaseData)
-headers=reader.next()  
+headers=next(reader)
 # print(headers)  
   
 featureList=[]  
@@ -30,3 +29,24 @@ dummyX=vec.fit_transform(featureList).toarray()
 print("dummyX:"+str(dummyX))  
 print(vec.get_feature_names())  
 print("labelList:"+str(labelList))  
+lb=preprocessing.LabelBinarizer()  
+dummyY=lb.fit_transform(labelList)  
+print("dummyY:"+str(dummyY))  
+  
+clf=tree.DecisionTreeClassifier(criterion='entropy')  
+clf=clf.fit(dummyX, dummyY)  
+print("clf:"+str(clf))  
+  
+with open("AllElectronics.dot",'w')as f:  
+    f=tree.export_graphviz(clf, feature_names=vec.get_feature_names(), out_file=f)  
+  
+#predict  
+oneRowX=dummyX[0,:]  
+print("oneRowX:"+str(oneRowX))  
+  
+newRowX=oneRowX  
+newRowX[0]=1  
+newRowX[2]=0  
+print("newRowX:"+str(newRowX))  
+predictedY=clf.predict([newRowX])  
+print("predictedY:"+str(predictedY))    
