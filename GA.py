c6726a85f407304647e97d5cae4841c7b00d0e52
JayFu <382603080@qq.com>
2018-05-09 23:41:35 +0200

    5.9

--- GA.py
+++ GA.py
@@ -188,7 +188,7 @@ class GeneticAlgorithm:
         return r
 
 if __name__ == '__main__':
-    # bounds = [[-3, 12.1], [4.1, 5.8]]
+    bounds = [[-3, 12.1], [4.1, 5.8]]
     precision = 100000
     algorithm = GeneticAlgorithm(bounds, precision, 0.01, 0.8, 100, 100)
     algorithm.ga()
