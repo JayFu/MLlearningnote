1bf2899dab7502ffd68efa948125e23a8a76310a
JayFu <382603080@qq.com>
2018-10-09 22:03:44 +0200

    测试新系统

--- normal  Cnn/CnnN.py
+++ normal  Cnn/CnnN.py
@@ -14,6 +14,7 @@ LOW_CASE = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
 # UP_CASE = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 
 #            'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
 #            'V', 'W', 'X', 'Y', 'Z']
+
 CAPTCHA_LIST = NUMBER + LOW_CASE
 CAPTCHA_LEN = 4
 CAPTCHA_HEIGHT = 32
